dfa76f87234f34f01b69312d3ea48cf9f9295db2
rise <rise.worlds@outlook.com>
2019-10-21 18:20:22 +0800

    sync

--- ios/PotatoCrypto.h
+++ ios/PotatoCrypto.h
@@ -1,9 +1,4 @@
-
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#else
-#import "Base/RCTBridgeModule.h"
-#endif
 
 @interface PotatoCrypto : NSObject <RCTBridgeModule>
 
